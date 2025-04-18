9a8df43e8b7e8dc8a9ae52b4081eddb3a97d7057
farnaz <whoisfarnaz@gmail.com>
2025-04-18 23:16:28 +0330

    add UserBehavior-Interface

---
+++ src/Spotify/UserBehavior.java
@@ -0,0 +1,7 @@
+package Spotify;
+
+public interface UserBehavior {
+    void createPlaylist (String Title, User Owner) throws InvalidOperationException;
+    void playMusic (Music music) throws InvalidOperationException;
+    void buyPremium (User owner, int month);
+}
